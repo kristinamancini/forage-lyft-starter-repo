bfec4165875180a0023d4cefddf688d55d8bdbc0
Kristina Mancini <km36558p@pace.edu>
2023-04-20 09:49:16 -0400

    Modified car.py

--- car.py
+++ car.py
@@ -2,8 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+
+    engine = Engine()
+    battery = Battery()
+
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
 
     @abstractmethod
     def needs_service(self):
